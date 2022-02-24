b5c9733995dbe1f09208cdf129e19d44e5426aa4
arifeen008 <feenkundee@gmail.com>
2022-02-24 12:05:33 +0700

    new update

--- application/views/index/about/vision.php
+++ application/views/index/about/vision.php
@@ -28,6 +28,9 @@
 			</div>
 		</div> -->
 	<div class="text-center">
-		<img class="img-fluid w-100" data-mdb-toggle="animation" data-mdb-animation="zoom-in" data-mdb-animation-start="onLoad" src="<?php echo base_url('picture/280506.jpg') ?>" />
+		<!-- <img class="img-fluid w-100" data-mdb-toggle="animation" data-mdb-animation="zoom-in" data-mdb-animation-start="onLoad" src="<?php echo base_url('picture/280506.jpg') ?>" /> -->
+		<img class="img-fluid w-100" data-mdb-toggle="animation" data-mdb-animation="zoom-in" data-mdb-animation-start="onLoad" src="<?php echo base_url('picture/วิสัยทัสเว็บ.jpg') ?>" />
+		<img class="img-fluid w-100" data-mdb-toggle="animation" data-mdb-animation="zoom-in" data-mdb-animation-start="onLoad" src="<?php echo base_url('picture/ค่านิยมเว็บ.jpg') ?>" />
+		<img class="img-fluid w-100" data-mdb-toggle="animation" data-mdb-animation="zoom-in" data-mdb-animation-start="onLoad" src="<?php echo base_url('picture/ยุธศาสตร์เว็บ.jpg') ?>" />
 	</div>
 </div>
