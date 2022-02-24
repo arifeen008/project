ea1f98058ba9f09bba33bf05184c3e10b4cac322
arifeen008 <feenkundee@gmail.com>
2022-02-24 12:00:51 +0700

    new update

--- application/views/index/project/gasstation.php
+++ application/views/index/project/gasstation.php
@@ -1,6 +1,6 @@
 <div class="container my-2">
 	<div class="lightbox">
-		<div class="row">
+		<!-- <div class="row">
 			<div class="col-md-4 my-2">
 				<img src="<?php echo base_url('picture/007.jpg') ?>" data-mdb-img="<?php echo base_url('picture/007.jpg') ?>" class="w-100 shadow-1-strong rounded" />
 			</div>
@@ -18,6 +18,26 @@
 			</div>
 			<div class="col-md-4 my-2">
 				<img src="<?php echo base_url('picture/012.jpg') ?>" data-mdb-img="<?php echo base_url('picture/012.jpg') ?>" class="w-100 shadow-1-strong rounded" />
+			</div>
+		</div> -->
+		<div class="row">
+			<div class="col-md-4 my-2">
+				<img src="<?php echo base_url('picture/1001.jpg') ?>" data-mdb-img="<?php echo base_url('picture/1001.jpg') ?>" class="w-100 shadow-1-strong rounded" />
+			</div>
+			<div class="col-md-4 my-2">
+				<img src="<?php echo base_url('picture/1002.jpg') ?>" data-mdb-img="<?php echo base_url('picture/1002.jpg') ?>" class="w-100 shadow-1-strong rounded" />
+			</div>
+			<div class="col-md-4 my-2">
+				<img src="<?php echo base_url('picture/1003.jpg') ?>" data-mdb-img="<?php echo base_url('picture/1003.jpg') ?>" class="w-100 shadow-1-strong rounded" />
+			</div>
+			<div class="col-md-4 my-2">
+				<img src="<?php echo base_url('picture/1004.jpg') ?>" data-mdb-img="<?php echo base_url('picture/1004.jpg') ?>" class="w-100 shadow-1-strong rounded" />
+			</div>
+			<div class="col-md-4 my-2">
+				<img src="<?php echo base_url('picture/1005.jpg') ?>" data-mdb-img="<?php echo base_url('picture/1005.jpg') ?>" class="w-100 shadow-1-strong rounded" />
+			</div>
+			<div class="col-md-4 my-2">
+				<img src="<?php echo base_url('picture/1006.jpg') ?>" data-mdb-img="<?php echo base_url('picture/1006.jpg') ?>" class="w-100 shadow-1-strong rounded" />
 			</div>
 		</div>
 	</div>
