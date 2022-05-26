de6f27354d7d6447ff46fbf781d4a1be403c1840
arifeen008 <feenkundee@gmail.com>
2022-05-26 14:31:20 +0700

    New config

--- application/views/officer/performance/performance.php
+++ application/views/officer/performance/performance.php
@@ -6,25 +6,6 @@ include_once("application/libraries/Thaidate.php");
 		<div class="card-body text-dark">
 			<div class="d-flex flex-row-reverse"><a href="<?php echo site_url('officer/upload_document') ?>" class="btn btn-success"><i class="fas fa-plus me-2"></i>เพิ่มเอกสาร</a></div>
 			<h5 class="card-title border-bottom">ผลการดำเนินงานประจำปี</h5>
-			<!-- <div class="lightbox">
-				<div class="row">
-					<div class="col-lg-4 my-2">
-						<img src="<?php echo base_url('picture/messageImage_1643253382476.jpg') ?>" data-mdb-img="<?php echo base_url('picture/messageImage_1643253382476.jpg') ?>" class="w-100" />
-					</div>
-					<div class="col-lg-4 my-2">
-						<img src="<?php echo base_url('picture/messageImage_1643253341557.jpg') ?>" data-mdb-img="<?php echo base_url('picture/messageImage_1643253341557.jpg') ?>" class="w-100" />
-					</div>
-					<div class="col-lg-4 my-2">
-						<img src="<?php echo base_url('picture/messageImage_1643253352261.jpg') ?>" data-mdb-img="<?php echo base_url('picture/messageImage_1643253352261.jpg') ?>" class="w-100" />
-					</div>
-					<div class="col-lg-4 my-2">
-						<img src="<?php echo base_url('picture/messageImage_1643253364924.jpg') ?>" data-mdb-img="<?php echo base_url('picture/messageImage_1643253364924.jpg') ?>" class="w-100" />
-					</div>
-					<div class="col-lg-4 my-2">
-						<img src="<?php echo base_url('picture/messageImage_1643253626653.jpg') ?>" data-mdb-img="<?php echo base_url('picture/messageImage_1643253626653.jpg') ?>" class="w-100" />
-					</div>
-				</div>
-			</div> -->
 			<div class="datatable" data-mdb-hover="true" data-mdb-full-pagination="true">
 				<table>
 					<thead>
