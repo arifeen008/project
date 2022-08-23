4fb24caf031a8cdde0e6594f38ddcdd0c0a7c71d
arifeen008 <feenkundee@gmail.com>
2022-08-23 20:54:22 +0700

    เพิ่ม Google analytic กับ tag manager

--- application/views/containner/head.php
+++ application/views/containner/head.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
-
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php echo $title ?></title>
-	<link rel="icon" href="<?php echo base_url('picture/sakofag-logo.png') ?>" type="image/gif" sizes="18x18">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
-	<link rel="stylesheet" href=<?php echo base_url('mdb5pro/css/mdb.min.css'); ?> type="text/css" />
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Sarabun:ital,wght@0,200;1,200&display=swap" rel="stylesheet">
-	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-	<!-- Google tag (gtag.js) -->
+	<script>
+		(function(w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({
+				'gtm.start': new Date().getTime(),
+				event: 'gtm.js'
+			});
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src =
+				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-PV5RFBP');
+	</script>
 	<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y7M3HX122N"></script>
 	<script>
 		window.dataLayer = window.dataLayer || [];
@@ -25,5 +31,13 @@
 
 		gtag('config', 'G-Y7M3HX122N');
 	</script>
+	<link rel="icon" href="<?php echo base_url('picture/sakofag-logo.png') ?>" type="image/gif" sizes="18x18">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
+	<link rel="stylesheet" href=<?php echo base_url('mdb5pro/css/mdb.min.css'); ?> type="text/css" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Sarabun:ital,wght@0,200;1,200&display=swap" rel="stylesheet">
+	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 	<?php header("Cache-Control: public, max-age=1000, s-maxage=1000"); ?>
 </head>
