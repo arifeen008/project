9e943a4978889c992dce800772b09ef835acede0
arifeen008 <feenkundee@gmail.com>
2022-09-26 13:58:52 +0700

    Up

--- application/views/index/contact/withus.php
+++ application/views/index/contact/withus.php
@@ -1,8 +1,31 @@
-<div class="container my-3">
+<div class="container mt-3">
 	<div class="card">
 		<div class="card-body text-center">
 			<p style="font-family: 'Chonburi', cursive;color:black;font-size: 40px;">ร่วมงานกับเรา</p>
-			<a href="<?php echo base_url(); ?>file/form/ใบสมัครงาน.pdf" class="btn btn-outline-success text-dark btn-block btn-lg" target="_blank"><p style="font-family: 'Sarabun', cursive;color:black;font-size: 20px;">ดาวน์โหลดใบสมัครงาน<i class="fas fa-download ms-3"></i></p> </a>
+			<a href="<?php echo base_url(); ?>file/form/ใบสมัครงาน.pdf" class="btn btn-outline-success text-dark btn-block btn-lg" target="_blank">
+				<p style="font-family: 'Sarabun', cursive;color:black;font-size: 20px;">ดาวน์โหลดใบสมัครงาน<i class="fas fa-download ms-3"></i></p>
+			</a>
 		</div>
 	</div>
 </div>
+
+<!-- <div class="container mt-5">
+	<section>
+		<div class="row">
+			<div class="col-lg-5 mb-4">
+				<div class="view overlay mb-4">
+					<img class="img-fluid mx-auto" src="https://mdbootstrap.com/img/Mockups/Transparent/Small/iphone-admin.png" alt="Sample image">
+					<a>
+						<div class="mask rgba-white-slight"></div>
+					</a>
+				</div>
+			</div>
+			<div class="col-lg-7 mb-4 align-self-center text-center text-lg-left">
+				<h2 class="h2 mb-4">Now Available</h2>
+				<p class="text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate esse quasi, veritatis totam voluptas nostrum quisquam eum porro a pariatur veniam. Fugit, error amet numquam iure provident voluptate esse quasi.</p>
+				<button type="button" class="btn btn-outline-dark waves-effect btn-md ml-lg-0"><i class="fab fa-apple fa-lg pr-1"></i> App Store</button>
+				<button type="button" class="btn btn-outline-dark waves-effect btn-md"><i class="fab fa-google-play fa-lg pr-1"></i> Google Play</button>
+			</div>
+		</div>
+	</section>
+</div> -->
