6358894309a491473fb6149ee9f61422ee0cd0a0
arifeen008 <feenkundee@gmail.com>
2022-09-29 14:07:34 +0700

    Delete web analytic

--- application/views/container/script.php
+++ application/views/container/script.php
@@ -2,11 +2,3 @@
 <script type="text/javascript" src="https://counter9.stat.ovh/private/counter.js?c=3wrcpuf5jgrke9993w3x3p7wsppfre13&down=async" async></script>
 <noscript><a href="https://www.freecounterstat.com" title="free hit counter"><img src="https://counter9.stat.ovh/private/freecounterstat.php?c=3wrcpuf5jgrke9993w3x3p7wsppfre13"></a></noscript>
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y7M3HX122N"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-    gtag('config', 'G-Y7M3HX122N');
-</script>
