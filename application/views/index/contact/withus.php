9db5cbf497edc5ef58f362cf74153bdd240d3554
arifeen008 <feenkundee@gmail.com>
2022-10-20 09:41:56 +0700

    Change withus

--- application/views/index/contact/withus.php
+++ application/views/index/contact/withus.php
@@ -1,48 +1,12 @@
-<!-- <div class="container mt-3">
-	<div class="card">
-		<div class="card-body text-center">
-			<p style="font-family: 'Chonburi', cursive;color:black;font-size: 40px;">ร่วมงานกับเรา</p>
-			<a href="<?php echo base_url(); ?>file/form/ใบสมัครงาน.pdf" class="btn btn-outline-success text-dark btn-block btn-lg" target="_blank">
-				<p style="font-family: 'Sarabun', cursive;color:black;font-size: 20px;">ดาวน์โหลดใบสมัครงาน<i class="fas fa-download ms-3"></i></p>
-			</a>
-		</div>
-	</div>
-</div> -->
-
-<!-- <div class="container mt-5">
-	<section>
-		<div class="row">
-			<div class="col-lg-5 mb-4">
-				<div class="view overlay mb-4">
-					<img class="img-fluid mx-auto" src="https://mdbootstrap.com/img/Mockups/Transparent/Small/iphone-admin.png" alt="Sample image">
-					<a>
-						<div class="mask rgba-white-slight"></div>
-					</a>
-				</div>
-			</div>
-			<div class="col-lg-7 mb-4 align-self-center text-center text-lg-left">
-				<h2 class="h2 mb-4">Now Available</h2>
-				<p class="text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate esse quasi, veritatis totam voluptas nostrum quisquam eum porro a pariatur veniam. Fugit, error amet numquam iure provident voluptate esse quasi.</p>
-				<button type="button" class="btn btn-outline-dark waves-effect btn-md ml-lg-0"><i class="fab fa-apple fa-lg pr-1"></i> App Store</button>
-				<button type="button" class="btn btn-outline-dark waves-effect btn-md"><i class="fab fa-google-play fa-lg pr-1"></i> Google Play</button>
-			</div>
-		</div>
-	</section>
-</div> -->
-
-<!--Section: Design Block-->
 <section>
-
-	<div class="bg-image bg-fixed text-center" style="background-image:url('https://mdbootstrap.com/img/Photos/Horizontal/City/12-col/img%20%2824%29.jpg'); height: 100vh;">
+	<div class="bg-image bg-fixed text-center" style="background-image:url('<?php echo base_url('picture/445.jpg') ?>'); height: 100vh;">
 		<div class="mask" style="background-color: rgba(63,81,181,.3);">
 			<div class="d-flex justify-content-center align-items-center h-100">
 				<div data-mdb-toggle="animation" data-mdb-animation="fade-in" data-mdb-animation-start="onLoad" class="text-white">
-					<p class="display-6 mb-5" style="font-family: 'Kanit';">ร่วมงานกับเรา</p>
-					<a class="btn btn-outline-light" role="button" href="<?php echo base_url('file/form/ใบสมัครงาน.pdf') ?>" style="font-family: 'Kanit';">ดาวน์โหลดใบสมัครงาน</a>
+					<p class="text-dark fs-1 mb-5" style="font-family: 'Kanit';">ร่วมงานกับเรา</p>
+					<a class="btn btn-outline-dark fs-2" role="button" href="<?php echo base_url('file/form/ใบสมัครงาน.pdf') ?>" style="font-family: 'Kanit';">ดาวน์โหลดใบสมัครงาน</a>
 				</div>
 			</div>
 		</div>
 	</div>
-
 </section>
-<!--Section: Design Block-->
