5326a97818b81fbb71990ba57c29f552341393b9
arifeen008 <feenkundee@gmail.com>
2022-11-22 13:04:46 +0700

    Up picture

--- application/views/index_test.php
+++ application/views/index_test.php
@@ -1,4 +1,4 @@
-<div class="card">
+<!-- <div class="card">
     <b class="card-title text-dark border-bottom border-secondary mx-2">ข่าวสาร</b>
     <div class="row">
         <?php foreach ($google_news->articles as $row) { ?>
@@ -12,5 +12,20 @@
             </div>
         <?php } ?>
     </div>
-</div>
+</div> -->
 
+<div class="card">
+    <b class="card-title text-dark border-bottom border-secondary mx-2">ข่าวสาร</b>
+    <div class="row">
+        <?php foreach (array_slice($google_news->results, 0, 8) as $row) { ?>
+            <div class="col-md-3">
+                <a href="<?php echo $row->link ?>" class="card hover-shadow mx-2 mb-1">
+                    <img src="<?php echo $row->image_url ?>" class="card-img-top" />
+                    <div class="card-body">
+                        <p class="card-text text-dark"><?php echo $row->title ?></p>
+                    </div>
+                </a>
+            </div>
+        <?php } ?>
+    </div>
+</div>
