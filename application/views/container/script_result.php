d6642b3f12819b3c2cb7f89d10a185560e76a0e5
arifeen008 <feenkundee@gmail.com>
2023-02-06 16:08:42 +0700

    Add other bar

--- application/views/container/script_result.php
+++ application/views/container/script_result.php
@@ -2,46 +2,116 @@
 <script type="text/javascript" src="https://counter4.optistats.ovh/private/counter.js?c=1wrapynwcwcsad47zfjfdme3eyy932su&down=async" async></script>
 <noscript><a href="https://www.freecounterstat.com"><img src="https://counter4.optistats.ovh/private/freecounterstat.php?c=1wrapynwcwcsad47zfjfdme3eyy932su"></a></noscript>
 <script>
-  // Data
-  const dataMixedChartExample = {
+  const memberBar = {
     type: 'bar',
     data: {
       labels: ['กระบี่', 'คลองยาง', 'อ่าวลึก', 'กาญจนดิษฐ์', 'คลองท่อม', 'ห้วยลึก', 'เกาะลันตา'],
-      datasets: [
-        // First dataset (bar)
-        {
-          label: 'ชาย',
-          data: [2112, 2343, 2545, 3423, 2365, 1985, 987],
-          order: 2,
-        },
-        // Second dataset (line)
-        {
-          label: 'หญิง',
-          data: [211, 2543, 2745, 3123, 2765, 1485, 587],
-          type: 'bar',
-          order: 1,
-          backgroundColor: 'rgba(66, 133, 244, 0.0)',
-          borderColor: '#94DFD7',
-          borderWidth: 2,
-          pointBorderColor: '#94DFD7',
-          pointBackgroundColor: '#94DFD7',
-          lineTension: 0.0,
-        },
-        {
-          label: 'ฟ',
-          data: [211, 2543, 2745, 3123, 2765, 1485, 587],
-          type: 'bar',
-          order: 1,
-          backgroundColor: 'rgba(66, 133, 244, 0.0)',
-          borderColor: '#94DFD7',
-          borderWidth: 2,
-          pointBorderColor: '#94DFD7',
-          pointBackgroundColor: '#94DFD7',
-          lineTension: 0.0,
-        },
-      ],
+      datasets: [{
+        label: 'จำนวนสมาชิก',
+        data: [3120, 1956, 1792, 2217, 1119, 1422, 1727],
+        // backgroundColor : 'black',
+        // borderColor :'white'
+      }]
     },
   };
+  const optionmemberBar = {
+    options: {
+      title: {
+        display: true,
+        text: "ยอดสมาชิกสหกรณ์",
+        fontSize: 25,
+        fontColor: 'black'
+      }
+    }
+  }
 
-  new mdb.Chart(document.getElementById('chart-options-example'), dataMixedChartExample);
+  const accountmemberBar = {
+    type: 'bar',
+    data: {
+      labels: ['กระบี่', 'คลองยาง', 'อ่าวลึก', 'กาญจนดิษฐ์', 'คลองท่อม', 'ห้วยลึก', 'เกาะลันตา'],
+      datasets: [{
+        label: 'ทำได้',
+        data: [97, 24, 7, 16, 4, 39, 55],
+      }],
+    },
+  };
+  const optionaccountmemberBar = {
+    options: {
+      title: {
+        display: true,
+        text: "สมาชิกปีบัญชี 2566",
+        fontSize: 25,
+        fontColor: 'black'
+      }
+    }
+  }
+
+  const sharedBar = {
+    type: 'bar',
+    data: {
+      labels: ['กระบี่', 'คลองยาง', 'อ่าวลึก', 'กาญจนดิษฐ์', 'คลองท่อม', 'ห้วยลึก', 'เกาะลันตา'],
+      datasets: [{
+        label: 'เป้าเงิน',
+        data: [116000, 617200, 311430, 1898800, 116850, 1081000, 2298910],
+      }],
+    },
+  };
+  const optionsharedBar = {
+    options: {
+      title: {
+        display: true,
+        text: "ทุนเรือนหุ้นปีบัญชี 2566",
+        fontSize: 25,
+        fontColor: 'black'
+      }
+    }
+  }
+
+  const educationBar = {
+    type: 'bar',
+    data: {
+      labels: ['กระบี่', 'คลองยาง', 'อ่าวลึก', 'กาญจนดิษฐ์', 'คลองท่อม', 'ห้วยลึก', 'เกาะลันตา'],
+      datasets: [{
+        label: 'เป้าเงิน',
+        data: [170100, 512900, 113800, 36200, 56400, 215500, 128900],
+      }],
+    },
+  };
+  const optioneducationBar = {
+    options: {
+      title: {
+        display: true,
+        text: "เงินฝากประเภทเพี่อการศึกษา ระยะเวลา 1 ปี ปีบัญชี 2566",
+        fontSize: 25,
+        fontColor: 'black'
+      }
+    }
+  }
+
+  const hadjumrahBar = {
+    type: 'bar',
+    data: {
+      labels: ['กระบี่', 'คลองยาง', 'อ่าวลึก', 'กาญจนดิษฐ์', 'คลองท่อม', 'ห้วยลึก', 'เกาะลันตา'],
+      datasets: [{
+        label: 'เป้าเงิน',
+        data: [-122874, -26100, -78150, -55071, -216853, 84805, 445900],
+      }],
+    },
+  };
+  const optionhadjumrahBar = {
+    options: {
+      title: {
+        display: true,
+        text: " เงินฝากประเภทฮัจญ์และอุมเราะห์ ระยะเวลา 1 ปี ปีบัญชี 2566",
+        fontSize: 25,
+        fontColor: 'black'
+      }
+    }
+  }
+
+  new mdb.Chart(document.getElementById('member-balance'), memberBar, optionmemberBar);
+  new mdb.Chart(document.getElementById('target-member'), accountmemberBar, optionaccountmemberBar);
+  new mdb.Chart(document.getElementById('shared-bar'), sharedBar, optionsharedBar);
+  new mdb.Chart(document.getElementById('education-bar'), educationBar, optioneducationBar);
+  new mdb.Chart(document.getElementById('hadjumrah-bar'), hadjumrahBar, optionhadjumrahBar);
 </script>
--- application/views/index/about/result.php
+++ application/views/index/about/result.php
@@ -1,3 +1,40 @@
 <div class="container my-3">
-    <canvas id="chart-options-example"></canvas>
+    <div class="row">
+        <div class="col-2">
+            <div class="nav flex-column nav-pills text-center" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <a class="nav-link active" data-mdb-toggle="pill" href="#member" role="tab" aria-selected="true">ยอดสมาชิกสหกรณ์</a>
+                <a class="nav-link" data-mdb-toggle="pill" href="#yearaccount" role="tab" aria-selected="false">สมาชิกบัญชี</a>
+                <a class="nav-link" data-mdb-toggle="pill" href="#shared" role="tab" aria-selected="false"> ทุนเรือนหุ้น</a>
+                <a class="nav-link" data-mdb-toggle="pill" href="#education" role="tab" aria-selected="false">เงินฝากประเภทเพี่อการศึกษา</a>
+                <a class="nav-link" data-mdb-toggle="pill" href="#hadjumrah" role="tab" aria-selected="false">hadjandumrah</a>
+                <a class="nav-link" data-mdb-toggle="pill" href="#is1224" role="tab" aria-selected="false">is1224</a>
+                <a class="nav-link" data-mdb-toggle="pill" href="#takaful" role="tab" aria-selected="false">takaful</a>
+            </div>
+        </div>
+        <div class="col-10">
+            <div class="tab-content" id="v-pills-tabContent">
+                <div class="tab-pane fade show active" id="member" role="tabpanel">
+                    <canvas id="member-balance"></canvas>
+                </div>
+                <div class="tab-pane fade" id="yearaccount" role="tabpanel">
+                    <canvas id="target-member"></canvas>
+                </div>
+                <div class="tab-pane fade" id="shared" role="tabpanel">
+                    <canvas id="shared-bar"></canvas>
+                </div>
+                <div class="tab-pane fade" id="education" role="tabpanel">
+                    <canvas id="education-bar"></canvas>
+                </div>
+                <div class="tab-pane fade" id="hadjumrah" role="tabpanel">
+                <canvas id="hadjumrah-bar"></canvas>
+                </div>
+                <div class="tab-pane fade" id="is1224" role="tabpanel">
+                    is1224
+                </div>
+                <div class="tab-pane fade" id="takaful" role="tabpanel">
+                    takaful
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
