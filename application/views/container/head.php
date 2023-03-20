7bb6d9f59d73d9c78aaf91db6859e18917b996ff
arifeen008 <feenkundee@gmail.com>
2023-03-20 13:41:29 +0700

    .......................

--- application/views/container/head.php
+++ application/views/container/head.php
@@ -3,7 +3,6 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
 	<title><?php echo $title ?></title>
 	<link rel="icon" href="<?php echo base_url('picture/sakofag-logo.png') ?>" type="image/gif" sizes="18x18">
 	<link rel="stylesheet" href=<?php echo base_url('mdb5pro/css/mdb.min.css'); ?> type="text/css" />
@@ -14,6 +13,7 @@
 	<link href="https://fonts.googleapis.com/css2?family=Sarabun:ital,wght@0,200;1,200&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Kanit:wght@100;200&display=swap" rel="stylesheet">
 	<script src="//cdn.ckeditor.com/4.19.1/standard/ckeditor.js"></script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y7M3HX122N"></script>
 	<script>
 		window.dataLayer = window.dataLayer || [];
 
@@ -21,6 +21,7 @@
 			dataLayer.push(arguments);
 		}
 		gtag('js', new Date());
+
 		gtag('config', 'G-Y7M3HX122N');
 	</script>
 	<?php header("Cache-Control: public, max-age=1000, s-maxage=1000"); ?>
