a14800a5877146c1dc3d17387d2d9e7cafab9d07
arifeen008 <feenkundee@gmail.com>
2023-03-21 09:47:51 +0700

    ...

--- application/views/firstpage.php
+++ application/views/firstpage.php
@@ -4,7 +4,9 @@
             <div class="row my-md-5 py-lg-5 justify-content-center justify-content-lg-start align-items-center h-100">
                 <div class="col-lg-7 col-xl-6 d-grid gap-2">
                     <a href="<?php echo site_url('index/firstpage') ?>" type="button" class="btn btn-success btn-lg" data-mdb-ripple-color="success">เข้าหน้าเว็บไซต์</a>
-                    <a href="<?php echo base_url('picture/เวลารอมฎอน.jpg'); ?>" type="button" class="btn btn-success btn-lg" data-mdb-ripple-color="success" download>ดาวน์โหลดปฏิทินรอมฏอน 2566</a>
+                    <a href="<?php echo base_url('picture/เวลารอมฎอน.jpg'); ?>" type="button" class="btn btn-success btn-lg" data-mdb-ripple-color="success" download>ดาวน์โหลดปฏิทินรอมฏอน 2566 จังหวัดกระบี่</a>
+                    <a href="<?php echo base_url('picture/476-1.jpg'); ?>" type="button" class="btn btn-success btn-lg" data-mdb-ripple-color="success" download>ดาวน์โหลดปฏิทินรอมฏอน 2566 จังหวัดสุราษฏร์ธานี</a>
+                    <a href="<?php echo base_url('picture/476-2.jpg'); ?>" type="button" class="btn btn-success btn-lg" data-mdb-ripple-color="success" download>ดาวน์โหลดปฏิทินรอมฏอน 2566 จังหวัดตรัง</a>
                 </div>
                 <div class="col-md-11 col-lg-5 offset-xl-1">
                     <img src="<?php echo base_url('picture/833155.jpg') ?>" class="img-fluid mt-4">
