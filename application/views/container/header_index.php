ddbe75dbed7b530c3496688832cea04b72a02801
arifeen008 <feenkundee@gmail.com>
2023-04-25 10:22:41 +0700

    .

--- application/views/container/header_index.php
+++ application/views/container/header_index.php
@@ -26,7 +26,7 @@
 </style>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 	<div class="container-fluid">
-		<a class="navbar-brand" href="<?php echo site_url('index/firstpage') ?>"><img src="<?php echo base_url('picture/logo-web1.jpg'); ?>"></a>
+		<a class="navbar-brand" href="<?php echo site_url('index') ?>"><img src="<?php echo base_url('picture/logo-web1.jpg'); ?>"></a>
 		<button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
 			<i class="fas fa-bars text-dark"></i>
 		</button>
